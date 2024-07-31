fd699516855d7f0accd25fff450f88693c2bd5b3
Alan Devins <alandevinsgamedev@gmail.com>
2024-07-31 13:54:22 +0100

    optimisation

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Trocaire-1.0";
 const contentToCache = [
-    "Build/Trocaire_WebGL.loader.js",
-    "Build/Trocaire_WebGL.framework.js",
-    "Build/Trocaire_WebGL.data",
-    "Build/Trocaire_WebGL.wasm",
+    "Build/alandevinsTUS.github.io.loader.js",
+    "Build/alandevinsTUS.github.io.framework.js",
+    "Build/alandevinsTUS.github.io.data",
+    "Build/alandevinsTUS.github.io.wasm",
     "TemplateData/style.css"
 
 ];
